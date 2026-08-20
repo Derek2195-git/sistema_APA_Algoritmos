ab07fd7e96009d75741c959d779a9fea54b263b0
LeslyLopez1 <158329281+LeslyLopez1@users.noreply.github.com>
2026-08-20 14:09:30 -0700

    Add files via upload

---
+++ src/Controlador/MainFX.java
@@ -0,0 +1,10 @@
+package Controlador;
+
+import Vista.VistaFX;
+
+//Main para abrir la ventana gradica
+public class MainFX {
+    public static void main(String[] args) {
+        VistaFX.main(args);
+    }
+}
